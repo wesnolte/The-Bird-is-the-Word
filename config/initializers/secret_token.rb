6bac96f51f9791d0e449bf236ebb186358a67d81
Wes <wwnolte@gmail.com>
2011-10-14 13:08:36 +0100

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebirdistheworde::Application.config.secret_token = '172b79098de231b459c29404bdf50dde14436c8df7e310dc951dc5729841cc43f8e935f602ade5fb1a5eaa4755448bc63f0dbee82beb5364b6b46e4355f4e046'
