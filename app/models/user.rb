04f87ab6bf97dc7a7cd2d1fd135645afcb918289
Wes <wwnolte@gmail.com>
2011-10-15 10:26:13 +0100

    trying to parse nested resources via api

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   
   has_one :score
   accepts_nested_attributes_for :score
+  alias score_attributes :score
   
+  def to_xml(options ={})
+    super(options.merge({:methods => :score_attributes}))
+  end
   
 end
